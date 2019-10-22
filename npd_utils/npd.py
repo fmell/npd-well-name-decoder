510dd0a0113f33aa06e74ff5443ddc42fe37af19
Fredrik Mellemstrand <fmell@equinor.com>
2019-10-22 11:13:23 +0200

    add parse_wellbore_name

---
+++ npd_utils/npd.py
@@ -0,0 +1,35 @@
+import re
+
+WELLBORE_REGEX = r"(\d{1,4})\/(\d{1,2})(-\w{1,2})*-(\d{1,2})( \w{1,2})*"
+
+
+def parse_wellbore_name(txt):
+    """Parse wellbore name
+        Args:
+            txt: str with wellbore name
+        Returns:
+            wellbores: dict with parsed wellbore name properties
+                keys: quadrant, block, wellbore_id, well_number, well_type
+    """
+    mo = re.match(WELLBORE_REGEX, txt)
+    if mo:
+        quadrant, block, wellbore_id, well_number, well_type = mo.groups()
+        if quadrant:
+            quadrant = int(quadrant)
+        if block:
+            block = int(block)
+        if wellbore_id:
+            wellbore_id = wellbore_id[1:]
+        if well_number:
+            well_number = int(well_number)
+        if well_type:
+            well_type = well_type[1:]
+        return {
+            "quadrant": quadrant,
+            "block": block,
+            "wellbore_id": wellbore_id,
+            "well_number": well_number,
+            "well_type": well_type,
+        }
+    else:
+        return None
